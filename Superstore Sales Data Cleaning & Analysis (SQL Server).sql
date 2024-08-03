a0da9520bc87dd24ca619bc0ee9b547beeb6c4e6
sslegoale0 <sslegoale0@gmail.com>
2024-08-04 01:46:53 +0200

    Add files via upload

---
+++ Superstore Sales Data Cleaning & Analysis (SQL Server).sql
@@ -0,0 +1,26 @@
+/* Superstore Sales Data Cleaning */
+
+CREATE DATABASE superstore_sales;
+
+
+
+USE superstore_sales;
+
+
+
+SELECT *
+FROM orders;
+
+
+
+SELECT *
+FROM people;
+
+
+
+SELECT *
+FROM [returns];
+
+
+
+/* 1. Removal of duplicate rows. */
